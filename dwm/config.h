be80be287d656318f0959bf976223cb9b6754ef7
zachuri <punsalang.zachary@gmail.com>
2022-03-20 22:07:48 -0700

    added warp patch(move mouse when change monitor)/ remove dwm-fullgaps patch/ NOTE: make changes to config.def.h (make file creates config.h)

--- dwm/config.h
+++ dwm/config.h
@@ -133,4 +133,3 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
