6a403e2bd432bb9a0f128119467eb257d97897e7
zachuri <punsalang.zachary@gmail.com>
2022-03-24 03:14:16 -0700

    configured dwm with new font and only 5 tags

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -6,8 +6,8 @@ static const unsigned int gappx     = 2;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10", "fontawesome:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "CaskaydiaCove NF:size=9", "fontawesome:size=9" };
+static const char dmenufont[]       = "CaskaydiaCove NF:size=9";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -22,7 +22,8 @@ static const char *colors[][3]      = {
 
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+//static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4", "5" };
 
 static const Rule rules[] = {
 	/* xprop(1):
